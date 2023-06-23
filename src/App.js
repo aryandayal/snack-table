9043394671c63606b8f43fc716ec604de9257332
aryandayal <dayal.rn@gmail.com>
2023-06-23 16:03:36 +0000

    initialization

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import snacks from './data/tableData';
 
-function App() {
+
+  function App() {
+    const [tableData, setTableData] = useState(snacks);
+    const [initialData, setInitialData] = useState(snacks);
+    const [search, setSearch] = useState("");
+    const handleSearch = (e) => {
+      const searchValue = e.target.value;
+      setSearch(searchValue);
+      if(searchValue) {
+          const filterData = initialData.filter(({product_name, ingredients}) => {
+              if(product_name.toLowerCase().includes(searchValue.toLowerCase())) {
+                  return true;
+              }
+              return ingredients.some((ingredient) => ingredient.toLowerCase().includes(searchValue.toLowerCase()));
+          });
+          setTableData(filterData);
+      } else {
+          setTableData(initialData);
+      }
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h2>Snack Table</h2>
+      <input placeholder='search with products & ingredients' value={search} onChange={handleSearch}/>
+      <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Product Name</th>
+          <th>Product Weight</th>
+          <th>Price (INR)</th>
+          <th>Calories</th>
+          <th>Ingredients</th>
+        </tr>
+        </thead>
+        <tbody>
+        {
+            (tableData?.length > 0) ? (
+                tableData?.map((snack) => (
+                <tr key={snack.id}>
+                    <td>{snack.id}</td>
+                    <td>{snack.product_name}</td>
+                    <td>{snack.product_weight}</td>
+                    <td>{snack.price}</td>
+                    <td>{snack.calories}</td>
+                    <td>{snack.ingredients.join(", ")}</td>
+                </tr>
+                ))
+            ) : (
+                <tr>
+                    <td colSpan={6}>
+                        Search products not found!
+                    </td>
+                </tr>
+            )
+        }
+        </tbody>
+      </table>
     </div>
+
   );
 }
 
